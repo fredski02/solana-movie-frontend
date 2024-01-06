f5e6029d364783a41b1bb233d20c78100afd13bd
fredski02 <frederico02@hotmail.com>
2024-01-06 17:23:45 +0000

    progress

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,15 +1,21 @@
 import { Card } from './Card'
-import { FC, useEffect, useMemo, useState } from 'react'
+import { FC, useEffect, useMemo, useState, ChangeEvent } from 'react'
 import { Movie } from '../models/Movie'
 import * as web3 from '@solana/web3.js'
 import { MovieCoordinator } from '../coordinators/MovieCoordinator'
 import { Button, Center, HStack, Input, Spacer } from '@chakra-ui/react'
+import { useDebounce } from 'usehooks-ts'
 
 export const MovieList: FC = () => {
-    const connection = new web3.Connection(web3.clusterApiUrl('devnet'))
     const [movies, setMovies] = useState<Movie[]>([])
     const [page, setPage] = useState(1)
     const [search, setSearch] = useState('')
+    const debouncedSearch = useDebounce<string>(search, 500)
+
+    const connection = new web3.Connection(web3.clusterApiUrl('devnet'), {
+        disableRetryOnRateLimit: false,
+    })
+
 
     useEffect(() => {
         MovieCoordinator.fetchPage(
@@ -19,15 +25,19 @@ export const MovieList: FC = () => {
             search,
             search !== ''
         ).then(setMovies)
-    }, [page, search])
-    
+    }, [page, debouncedSearch])
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setSearch(event.target.value)
+    }
+
     return (
         <div>
             <Center>
                 <Input
                     id='search'
                     color='gray.400'
-                    onChange={event => setSearch(event.currentTarget.value)}
+                    onChange={handleChange}
                     placeholder='Search'
                     w='97%'
                     mt={2}
--- coordinators/MovieCoordinator.ts
+++ coordinators/MovieCoordinator.ts
@@ -8,37 +8,44 @@ export class MovieCoordinator {
     static accounts: web3.PublicKey[] = []
 
     static async prefetchAccounts(connection: web3.Connection, search: string) {
+        console.log('search string is ', search)
         const accounts = await connection.getProgramAccounts(
             new web3.PublicKey(MOVIE_REVIEW_PROGRAM_ID),
             {
                 dataSlice: { offset: 2, length: 18 },
                 filters: search === '' ? [] : [
-                    { 
-                        memcmp: 
-                            { 
-                                offset: 6, 
-                                bytes: bs58.encode(Buffer.from(search))
-                            }
+                    {
+                        memcmp:
+                        {
+                            offset: 6,
+                            bytes: bs58.encode(Buffer.from(search))
+                        }
                     }
-                ]
-            }
+                ],
+            },
         )
 
-        accounts.sort( (a, b) => {
+        accounts.sort((a, b) => {
             const lengthA = a.account.data.readUInt32LE(0)
             const lengthB = b.account.data.readUInt32LE(0)
             const dataA = a.account.data.slice(4, 4 + lengthA)
             const dataB = b.account.data.slice(4, 4 + lengthB)
+
             return dataA.compare(dataB)
         })
 
         this.accounts = accounts.map(account => account.pubkey)
     }
 
-    static async fetchPage(connection: web3.Connection, page: number, perPage: number, search: string, reload: boolean = false): Promise<Movie[]> {
+    static async fetchPage(connection: web3.Connection, page: number, perPage: number, search: string, reload : boolean = false): Promise<Movie[]> {
+        console.log('search string is ', search)
+
         if (this.accounts.length === 0 || reload) {
+            console.log('getting accounts')
             await this.prefetchAccounts(connection, search)
         }
+
+        console.log('getting data')
 
         const paginatedPublicKeys = this.accounts.slice(
             (page - 1) * perPage,
